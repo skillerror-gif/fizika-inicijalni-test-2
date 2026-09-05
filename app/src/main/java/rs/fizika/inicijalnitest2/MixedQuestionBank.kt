b7210614f2a556ffeb5dc6f88075ed627e5d87b2
skillerror-gif <branevukov10@gmail.com>
2026-09-05 23:36:21 +0200

    Include six random calculation problems in each test

--- app/src/main/java/rs/fizika/inicijalnitest2/MixedQuestionBank.kt
+++ app/src/main/java/rs/fizika/inicijalnitest2/MixedQuestionBank.kt
@@ -3,8 +3,11 @@ package rs.fizika.inicijalnitest2
 object MixedQuestionBank {
 
     fun buildTest20(): List<Question> {
-        val easy = QuestionBank.easyQuestions().shuffled().take(10)
-        val hard = QuestionBank.hardQuestions().shuffled().take(10)
-        return (easy + hard).shuffled()
+        // 14 pitanja iz postojeće baze + 6 računskih zadataka.
+        // Računski deo: 3 osnovna + 3 srednja, nasumično iz nove baze od 70 zadataka.
+        val theoryEasy = QuestionBank.easyQuestions().shuffled().take(7)
+        val theoryHard = QuestionBank.hardQuestions().shuffled().take(7)
+        val calculations = CalculationQuestionBank.randomSix()
+        return (theoryEasy + theoryHard + calculations).shuffled()
     }
 }
