7c340316cb0c5ac6b27eb5c28001b2c53f44b9f3
skillerror-gif <branevukov10@gmail.com>
2026-09-05 23:06:27 +0200

    Use 20-question mixed tests without diagram questions

--- app/src/main/java/rs/fizika/inicijalnitest2/MixedQuestionBank.kt
+++ app/src/main/java/rs/fizika/inicijalnitest2/MixedQuestionBank.kt
@@ -2,7 +2,7 @@ package rs.fizika.inicijalnitest2
 
 object MixedQuestionBank {
 
-    fun buildTest30(): List<Question> {
+    fun buildTest20(): List<Question> {
         val easy = QuestionBank.easyQuestions().shuffled().take(10)
         val hard = QuestionBank.hardQuestions().shuffled().take(10)
         return (easy + hard).shuffled()
