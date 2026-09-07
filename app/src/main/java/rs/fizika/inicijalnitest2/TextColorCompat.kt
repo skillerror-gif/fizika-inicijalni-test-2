c5a5be51ec8d3b42c64955dc2ee09971bf1bc087
skillerror-gif <branevukov10@gmail.com>
2026-09-07 02:09:06 +0200

    Fix RadioButton text color overload

---
+++ app/src/main/java/rs/fizika/inicijalnitest2/TextColorCompat.kt
@@ -0,0 +1,9 @@
+package rs.fizika.inicijalnitest2
+
+import android.graphics.Color
+import android.widget.RadioButton
+
+@Suppress("UNUSED_PARAMETER")
+fun RadioButton.setTextColor(value: CharSequence?) {
+    setTextColor(Color.parseColor("#173246"))
+}
